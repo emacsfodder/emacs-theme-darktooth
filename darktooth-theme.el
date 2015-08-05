38511d530bd1db9b28e8fe221025c88f8c742be7
Jason Milkins <jason@opsmanager.com>
2015-08-05 09:54:14 +0800

    Amend shell mode colors

--- darktooth-theme.el
+++ darktooth-theme.el
@@ -4,7 +4,7 @@
 
 ;; Authors: Jasonm23 <jasonm23@gmail.com>
 ;; URL: http://github.com/emacsfodder/emacs-theme-darktooth
-;; Version: 0.1.27
+;; Version: 0.1.28
 
 ;;; Commentary:
 ;;  Color theme for Emacs, when soothe and gruvbox collide.
@@ -296,7 +296,8 @@
    ;; Use tip colors for the pos-tip color vars (see below)
 
    ;; sh mode
-   `(sh-heredoc                                ((t (:foreground ,darktooth-bright_blue :background nil))))
+   `(sh-heredoc                                ((t (:foreground ,darktooth-darkslategray4 :background nil))))
+   `(sh-quoted-exec                            ((t (:foreground ,darktooth-darkslategray4 :background nil))))
 
    ;; Company Mode
    `(company-echo                              ((t (:inherit company-echo-common ))))
