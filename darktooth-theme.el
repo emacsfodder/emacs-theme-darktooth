dde50661210d8610cd03526a6c9a922bb0e494a6
Jason Milkins <jasonm23@users.noreply.github.com>
2017-03-13 21:17:47 +1100

    Bump version

--- darktooth-theme.el
+++ darktooth-theme.el
@@ -4,7 +4,7 @@
 
 ;; Authors: Jason Milkins <jasonm23@gmail.com>
 ;; URL: http://github.com/emacsfodder/emacs-theme-darktooth
-;; Version: 0.3.4
+;; Version: 0.3.5
 ;; Package-Requires: ((autothemer "0.2"))
 
 ;;; Commentary:
