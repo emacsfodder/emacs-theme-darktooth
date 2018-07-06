e531beccd76e36161f3967fca1a2b1b96045115b
galaunay <gaby.launay@tutanota.com>
2018-07-06 21:06:39 +0100

    Finish adding erc to darktooth

--- darktooth-theme.el
+++ darktooth-theme.el
@@ -763,25 +763,27 @@
   ;; MODE SUPPORT: erc
   (erc-action-face                           (:inherit 'erc-default-face))
   (erc-bold-face                             (:weight 'bold))
-  (erc-current-nick-face                     (:foreground darktooth-bright_blue
+  (erc-current-nick-face                     (:foreground darktooth-aquamarine4
                                               :weight 'bold))
   (erc-dangerous-host-face                   (:inherit 'font-lock-warning-face))
   (erc-default-face                          (:inherit 'default))
   (erc-direct-msg-face                       (:inherit 'erc-default-face))
-  (erc-error-face                            (:inherit font-lock-warning-face))
-  (erc-fool-face                             (:inherit erc-default-face))
-  ;; (erc-input-face                            (:foreground ,zenburn-yellow))
-  ;; (erc-keyword-face ((t (:foreground ,zenburn-blue :weight bold))))
-  ;; (erc-nick-default-face ((t (:foreground ,zenburn-yellow :weight bold))))
-  ;; (erc-my-nick-face ((t (:foreground ,zenburn-red :weight bold))))
-  ;; (erc-nick-msg-face ((t (:inherit erc-default-face))))
-  ;; (erc-notice-face ((t (:foreground ,zenburn-green))))
-  ;; (erc-pal-face ((t (:foreground ,zenburn-orange :weight bold))))
-  ;; (erc-prompt-face ((t (:foreground ,zenburn-orange :background ,zenburn-bg :weight bold))))
-  ;; (erc-timestamp-face ((t (:foreground ,zenburn-green+4))))
-  ;; (erc-underline-face ((t (:underline t))))
-
-  )
+  (erc-error-face                            (:inherit 'font-lock-warning-face))
+  (erc-fool-face                             (:inherit 'erc-default-face))
+  (erc-input-face                            (:foreground darktooth-sienna))
+  (erc-my-nick-face                          (:foreground darktooth-sienna
+                                              :weight 'bold))
+  (erc-nick-msg-face                         (:inherit 'erc-default-face))
+  (erc-notice-face                           (:foreground darktooth-dark4))
+  (erc-timestamp-face                        (:foreground darktooth-neutral_green))
+  (erc-underline-face                        (:underline t))
+  (erc-prompt-face                           (:foreground darktooth-sienna
+                                              :weight 'bold))
+  (erc-pal-face                              (:foreground darktooth-neutral_yellow
+                                              :weight 'bold))
+  (erc-keyword-face                          (:foreground darktooth-bright_orange
+                                              :weight 'bold))
+  (erc-nick-default-face                     (:weight 'bold)))
 
  (defface darktooth-modeline-one-active
    `((t
